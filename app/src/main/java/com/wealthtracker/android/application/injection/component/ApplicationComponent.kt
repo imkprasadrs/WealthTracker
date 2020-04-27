cba5e1cf3df767f21489d35cab89e98414cabe9a
krishna090 <krishnaprasad.rs@locladz.com>
2020-04-27 17:53:29 +1000

    Firebase analytics with dagger implementation added

--- app/src/main/java/com/wealthtracker/android/application/injection/component/ApplicationComponent.kt
+++ app/src/main/java/com/wealthtracker/android/application/injection/component/ApplicationComponent.kt
@@ -1,10 +1,8 @@
 package com.wealthtracker.android.application.injection.component
 
-import com.wealthtracker.android.application.injection.module.ApplicationModule
-import com.wealthtracker.android.application.injection.module.NetworkModule
-import com.wealthtracker.android.application.injection.module.RetrofitModule
-import com.wealthtracker.android.application.injection.module.StocksModule
+import com.wealthtracker.android.application.injection.module.*
 import com.wealthtracker.android.data.account.WealthTrackerApplication
+import com.wealthtracker.android.domain.analytics.AnalyticsRepository
 import com.wealthtracker.android.domain.portfolio.PortfolioRepository
 import dagger.Component
 import javax.inject.Singleton
@@ -15,11 +13,16 @@ import javax.inject.Singleton
 @Singleton
 @Component(
     modules = arrayOf(
-        ApplicationModule::class, NetworkModule::class, RetrofitModule::class, StocksModule::class
+        ApplicationModule::class,
+        NetworkModule::class,
+        RetrofitModule::class,
+        StocksModule::class,
+        FirebaseModule::class
     )
 )
 interface ApplicationComponent {
     fun inject(application: WealthTrackerApplication)
 
     fun getPortfolioRepo(): PortfolioRepository
+    fun getAnalyticsRepo(): AnalyticsRepository
 }
--- app/src/main/java/com/wealthtracker/android/presentation/startup/presenter/SplashPresenter.kt
+++ app/src/main/java/com/wealthtracker/android/presentation/startup/presenter/SplashPresenter.kt
@@ -1,9 +1,11 @@
 package com.wealthtracker.android.presentation.startup.presenter
 
+import com.wealthtracker.android.domain.analytics.AnalyticsRepository
 import com.wealthtracker.android.presentation.base.Presenter
 import javax.inject.Inject
 
-class SplashPresenter @Inject constructor() : Presenter() {
+class SplashPresenter @Inject constructor(private val analyticsRepository: AnalyticsRepository) :
+    Presenter() {
 
     private lateinit var display: Display
     private lateinit var router: Router
@@ -13,6 +15,13 @@ class SplashPresenter @Inject constructor() : Presenter() {
         this.display = display
         this.router = router
     }
+    // endregion
+
+    // region public methods
+    fun pageOpened() {
+        analyticsRepository.logEvents("Splash Screen")
+    }
+    // endregion
 
     interface Display {
 
